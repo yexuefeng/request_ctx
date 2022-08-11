3a7b86737d5bfa02d0b67ac95ff9eb7248b2a0bf
yexuefeng <yexuefeng@bytedance.com>
2022-08-11 09:02:20 +0800

    add length limit of middlewares

--- request_ctx.go
+++ request_ctx.go
@@ -1,5 +1,11 @@
 package requestctx
 
+import "errors"
+
+const (
+	MaxMiddlewareNum = 63
+)
+
 // RequestCtx request context
 type RequestCtx struct {
 	index       int8
@@ -20,8 +26,12 @@ func (r *RequestCtx) Next() {
 	}
 }
 
-func (r *RequestCtx) Use(args ...func(r *RequestCtx)) {
+func (r *RequestCtx) Use(args ...func(r *RequestCtx)) error {
+	if len(r.middleWares)+len(args) > MaxMiddlewareNum {
+		return errors.New("too many middlewares")
+	}
 	r.middleWares = append(r.middleWares, args...)
+	return nil
 }
 
 func (r *RequestCtx) GetIndex() int8 {
